8f43acb4a69a86763373a1d0ae47d0a9dc553f64
anjalighare <anjalidesh1995@gmail.com>
2025-01-25 12:08:07 +0530

    sample commit 1

--- src/test/java/SampleTest2.java
+++ src/test/java/SampleTest2.java
@@ -1,10 +1,10 @@
 
-public class SampleTest {
+public class SampleTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("This is sample GitHub ");
+		System.out.println("This is sample GitHub Project ");
 		
 	}
 
